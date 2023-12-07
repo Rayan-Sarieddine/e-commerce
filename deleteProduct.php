967e1538c5c603b257cdff45c6a0b45dbdd34580
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-07 11:40:58 +0200

    implemented delete products

---
+++ deleteProduct.php
@@ -0,0 +1,36 @@
+<?php  
+header("Access-Control-Allow-Origin:*");
+include("connection.php");
+
+
+$barcode=$_POST['product_barcode'];
+
+$check_query = $mysqli->prepare('SELECT * FROM products WHERE product_barcode = ?');
+$check_query->bind_param('i', $barcode);
+$check_query->execute();
+$check_result = $check_query->get_result();
+$num_rows = $check_result->num_rows;
+$response=[];
+if($num_rows> 0){
+  $query=$mysqli->prepare('Delete FROM products Where product_barcode=?');
+  $query->bind_param('i',$barcode);
+  
+  if($query->execute()){
+  $response['success']=true;
+  $response['msg']= 'Product deleted';
+  } else{ 
+    $response['success']=false;
+    $response['msg']= 'Falied to delete product';
+  
+  }
+}
+else{
+
+  $response['success']=false;
+  $response['msg']= 'product does not exist';
+}
+
+
+
+echo json_encode($response);
+?>
